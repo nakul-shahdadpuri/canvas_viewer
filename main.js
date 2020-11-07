4b568b1b375c73c024971d8934ee920de6e0e835
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2020-11-07 23:11:59 +0530

    structure of frontend

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ const Window = electron.BrowserWindow;
 
 
 app.on("ready",() => {
-    let win = new Window();
+    let win = new Window({
+        width:600,
+        height:600,
+        minHeight:600,
+        minWidth:600,
+        maxHeight:600,
+        maxWidth:600,
+    });
     win.loadFile('index.html')
 })
