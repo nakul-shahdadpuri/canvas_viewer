177630ee3203dceea7a55aa953352fd979f53d18
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2020-11-11 21:15:12 +0530

    text editor made

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const electron = require('electron')
+const { ipcMain } = require('electron');
+const electron = require('electron');
+const fs = require('fs');
 
 const app = electron.app;
 const Window = electron.BrowserWindow;
+const {ipc} = electron;
 
 
 app.on("ready",() => {
@@ -12,6 +15,21 @@ app.on("ready",() => {
         minWidth:600,
         maxHeight:600,
         maxWidth:600,
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
     win.loadFile('index.html')
+})
+
+
+//communication with renderer
+
+ipcMain.on('save', (event,text)=> {
+    fs.writeFile('test.txt',text,(err) => {
+        if(err){
+            console.log('ERROR',err);
+        }
+        console.log('File saved')
+    });
 })
