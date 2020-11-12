802e8866a937be40fd41d3acbabeab1442a491ac
nakul-shahdadpuri <nakulshahdadpuri3141@gmail.com>
2020-11-12 20:54:22 +0530

    added save feature

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 const { ipcMain } = require('electron');
 const electron = require('electron');
 const fs = require('fs');
-
+const {dialog} = electron;
 const app = electron.app;
 const Window = electron.BrowserWindow;
 const {ipc} = electron;
 
+let win;
 
 app.on("ready",() => {
-    let win = new Window({
+    win = new Window({
         width:600,
         height:600,
         minHeight:600,
@@ -26,10 +27,11 @@ app.on("ready",() => {
 //communication with renderer
 
 ipcMain.on('save', (event,text)=> {
-    fs.writeFile('test.txt',text,(err) => {
+    let path = dialog.showSaveDialogSync(win,{defaultPath: "save.txt"});
+    fs.writeFile(path,text,(err)=>{
         if(err){
-            console.log('ERROR',err);
+            console.log("ERROR");
         }
-        console.log('File saved')
-    });
-})
+        console.log("File saved.")
+    })
+});
